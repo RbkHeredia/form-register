207d4c6d4ff569063595f750118c42cb39de6f0c
RbkHeredia <rbkheredia@gmail.com>
2023-03-22 12:54:07 -0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { createContext, useState  } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, 
-  Routes,
+import { Route, Routes,
 } from "react-router-dom";
 
 import Heather from './steps/heather';
@@ -19,22 +18,19 @@ function App() {
 
   const [data, setData] = useState({name:'', areas:[], skills:[], idioms:[{idiom:'Español', level:'Nativo'}]})
   return (
-    <Router>
       <DataContext.Provider value={[data, setData]} >
-        <div className="App">
+        {/* <div className="App"> */}
           <Heather/>
             <Routes>
-              <Route path='/' Component={RegistroFreelancers}/>
+              <Route exact path='/' Component={RegistroFreelancers}/>
                 <Route path='areas' Component={Areas}/>
                 <Route path='skills' Component={Skills}/>
                 <Route path='idioms' Component={Idioms}/>
                 <Route path='completed' Component={Completed}/>
               
             </Routes>
-          {/* <RouterProvider router={router}/> */}
-        </div>
+        {/* </div> */}
       </DataContext.Provider>
-    </Router>
   );
 }
 
